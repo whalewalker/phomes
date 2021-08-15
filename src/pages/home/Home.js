f05b7bb165cecca03c87485d8017c57dd15c2048
Gastt <olugbengagt@gmail.com>
2021-08-15 16:12:04 +0100

    Navbar in progress

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import HomeSection from "../../components/sections/home/HomeSection";
 import ChooseSection from "../../components/sections/choose/ChooseSection";
+import Footer from '../../components/sections/footer/Footer';
+
 
 const Home = () => {
     return (
         <>
             <HomeSection/>
             <ChooseSection/>
+            <Footer/>
+
         </>
     );
 };
