9e16e3490089a2be73ce81e472265ba71348934f
whalewalker <mongodbms@gmail.com>
2021-08-15 16:15:03 +0100

    updated

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import HomeSection from "../../components/sections/home/HomeSection";
+import AboutSection from "../../components/sections/about/AboutSection";
+import ChooseSection from "../../components/sections/choose/ChooseSection";
 
 const Home = () => {
     return (
         <>
             <HomeSection/>
+            <AboutSection/>
+            <ChooseSection/>
         </>
     );
 };
